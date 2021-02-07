72632d95b2908123855fb53553803e15b190b4b6
HoangTrang150302 <73728218+HoangTrang150302@users.noreply.github.com>
2021-02-07 21:18:57 +0700

    Delete A2.cpp

--- A2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	double x = (double) (3/5); //de ra ket qua la mot so dang double thi mot trong hai so phai co dang double
-	cout << x; //x bang 0
-	return 0;
-}
